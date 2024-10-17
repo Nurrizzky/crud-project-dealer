f312c7878362616ffd1ed8e12fc2f5f6cdcc24df
nurrizky <nurrizkyr90@gmail.com>
2024-10-18 00:02:21 +0700

    icon update

--- resources/views/dataMobil/create.blade.php
+++ resources/views/dataMobil/create.blade.php
@@ -17,27 +17,27 @@
             <h3 class="text-3xl font-semibold mb-5">Tambah data mobil.</h3>
                <div class="w-full h-full bg-black p-5 flex flex-col gap-5 rounded-md">
                   <div class="flex gap-3">
-                     <i class="ph-light ph-identification-badge text-4xl"></i>
+                     <i class="ph-light ph-car text-4xl"></i>
                      <input type="text" name="brand" id="brand" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none" placeholder="Masukkan nama brand">
                   </div>
                   <div class="flex gap-3">
-                     <i class="ph-light ph-envelope text-4xl"></i>
+                     <i class="ph-light ph-flag text-4xl"></i>
                      <input type="text" name="from" id="from" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none" placeholder="Masukkan Asal Mobil dibuat">
                   </div>
                   <div class="flex gap-3">
-                     <i class="ph-light ph-envelope text-4xl"></i>
+                     <i class="ph-light ph-sphere text-4xl"></i>
                      <input type="text" name="model" id="model" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none" placeholder="Masukkan model mobil">
                   </div>
                   <div class="flex gap-3">
-                     <i class="ph-light ph-envelope text-4xl"></i>
+                     <i class="ph-light ph-palette text-4xl"></i>
                      <input type="text" name="color" id="color" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none" placeholder="Masukkan warna mobil">
                   </div>
                   <div class="flex gap-3">
-                     <i class="ph-light ph-envelope text-4xl"></i>
+                     <i class="ph-light ph-timer text-4xl"></i>
                      <input type="number" name="years" id="years" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none" placeholder="Masukkan tahun mobil dibuat">
                   </div>
                   <div class="flex gap-3">
-                     <i class="ph-light ph-address-book-tabs text-4xl"></i>
+                     <i class="ph-light ph-gear text-4xl"></i>
                      <select name="transmission" id="transmission" class="w-full px-4 py-3 bg-[#111111] text-white rounded-lg border border-[#212121] focus:shadow-sm focus:shadow-gray-500 focus:outline-none">
                         <option hidden disabled selected>Pilih Role</option>
                         <option value="manual">manual</option>
